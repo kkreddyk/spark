76dbe6d29126199bcb7da865746cb8d651f34895
kkreddyk <kkreddy.k@gmail.com>
2022-02-23 01:04:24 -0800

    ..

--- s7 - Copy/src/main/scala/p7/s77.scala
+++ s7 - Copy/src/main/scala/p7/s77.scala
@@ -10,15 +10,16 @@ object s77 {
    println("oo999ll") 
    
    s.conf.getAll.foreach(println)
-   
+   import s.implicits._
    val df = s.read.format("csv").option("header","true").load(args(0))
    
    df.show()
    
    df.write.mode("Overwrite").save("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\1")
    
-   df.write.partitionBy("gender","City").mode("Overwrite").save("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\p1")
+   //df.write.partitionBy("gender","City").mode("Overwrite").save("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\p1")
    
+   df.select("gender","City").filter($"City" ==="Agassiz").write.partitionBy("gender").mode("Overwrite").save("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\p2")
    
    val tdf=s.read.load("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\1")
   println("Count="+tdf.count)
