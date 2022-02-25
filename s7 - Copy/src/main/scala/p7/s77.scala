770c3dfe89e84341a530ab2bdec66d2140b85bd8
kkreddyk <kkreddy.k@gmail.com>
2022-02-25 11:41:39 -0800

    ..

--- s7 - Copy/src/main/scala/p7/s77.scala
+++ s7 - Copy/src/main/scala/p7/s77.scala
@@ -19,17 +19,24 @@ object s77 {
    import s.implicits._
    val df = s.read.format("csv").option("header","true").load(args(0))
    
-   df.show()
+ df.createOrReplaceTempView("aa")
+ 
+ //s.sql("select * from aa where city='Abbotsford'").show(500,false)
+  
+ s.sql("select city,count(*),max(cast(age as integer)) from aa group by city having count(*) > 50 order by city").show(5,false)
    
-   df.write.mode("Overwrite").save("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\1")
+ s.sql("select city,count(*),max(cast(age as integer)) from aa group by city having count(*) < 50 order by city").explain
+  
+   
+   //df.write.mode("Overwrite").save("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\1")
    
    //df.write.partitionBy("gender","City").mode("Overwrite").save("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\p1")
    
-   df.select("gender","City").filter($"City" ==="Agassiz").write.partitionBy("gender").mode("Overwrite").save("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\p2")
+   //df.select("gender","City").filter($"City" ==="Agassiz").write.partitionBy("gender").mode("Overwrite").save("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\p2")
    
    val tdf=s.read.load("C:\\Datas\\Bigdata\\spark_data_files\\tgt\\parq\\1")
   println("Count="+tdf.count)
   
-  tdf.printSchema
+  //tdf.printSchema
   }
 }
