bb82318532195b038acd992b45e8753f06d2ac23
kkreddyk <kkreddy.k@gmail.com>
2022-03-04 12:31:06 -0800

    streaming - file

--- s7 - Copy/src/main/scala/p7/stream1.scala
+++ s7 - Copy/src/main/scala/p7/stream1.scala
@@ -6,48 +6,55 @@ import org.apache.spark.sql.functions._
 import org.apache.logging.log4j.Logger
 import org.apache.logging.log4j.LogManager
 
-
+import org.apache.spark.sql.types._
 
 object stream1 {
   
   
   def main(args: Array[String]){
     
-    println("starting")
-    
-    val s=SparkSession.builder().appName("Streaming").master("local[*]").getOrCreate()
-    
-    //s.conf.getAll.foreach(println)
-    
-    //s.sparkContext.setLogLevel("INFO")
+	  println("starting")
 
-  val logger: Logger =  LogManager.getLogger(this.getClass.getName);
-    
-     logger.info("Logger : Welcome to log4j")
-     
-    
+	  val s=SparkSession.builder().appName("Streaming").master("local[*]").getOrCreate()
 
-    logger.debug("This is debug message");
-        logger.info("This is info message");
-        logger.warn("This is warn message");
-        logger.fatal("This is fatal message");
-        logger.error("This is error message");
-       println("log level:::::"+ logger.getLevel())
+	  //s.conf.getAll.foreach(println)
 
-       
-       println("===="+this.getClass.getName)
-    
-       
-       //Thread.sleep(100000)
-    
-    println("Stream:::")
-    val initDF = s.readStream.format("rate").option("rowsPerSecond",1).load()
+	  //s.sparkContext.setLogLevel("INFO")
 
-    println("Is Streaming? :: " + initDF.isStreaming)
+	  val logger: Logger =  LogManager.getLogger(this.getClass.getName);
 
-    val resultDF = initDF.withColumn("result_________y", col("value") + lit(9))
-    
-    resultDF.writeStream.outputMode("append").option("truncate",false).format("console").start().awaitTermination()
+	  logger.info("Logger : Welcome to log4j")
+
+	  logger.debug("This is debug message");
+	  logger.info("This is info message");
+	  logger.warn("This is warn message");
+	  logger.fatal("This is fatal message");
+	  logger.error("This is error message");
+	  println("log level:::::"+ logger.getLevel())
+
+
+	  println("===="+this.getClass.getName)
+
+
+	  //Thread.sleep(100000)
+
+	  println("Stream:::")
+	  val initDF = s.readStream.format("rate").option("rowsPerSecond",1).load()
+	  //val initDF = s.readStream.format("rate").load
+
+	  //val st = new StructType().add("value", "string")
+	  //val initDF = s.readStream.format("file").option("delimiter",":").schema(st).csv("C:\\Datas\\Logs")
+
+
+	  initDF.printSchema
+	  println("Is Streaming? :: " + initDF.isStreaming)
+
+
+	  val resultDF = initDF.withColumn("result____x_____y", col("value") + lit(9))
+
+	  resultDF.writeStream.format("console").start().awaitTermination()
+
+	  //resultDF.writeStream.outputMode("append").option("truncate",false).format("console").start().awaitTermination()
 
 
   }
